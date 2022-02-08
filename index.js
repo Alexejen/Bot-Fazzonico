84a92ca421ac7dc12315302d6bdc59f3c236a0d4
Alexejen <98880262+Alexejen@users.noreply.github.com>
2022-02-08 11:48:48 +0100

    Bot Fazzonico
    comandi by LCFAlexis#7501

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const client = new Discord.Client(
     { intents: ["GUILDS", "GUILD_MEMBERS", "GUILD_MESSAGES", "GUILD_BANS"] }
 )
 
-client.login(process.token.env)
+client.login(process.env.token)
 
 client.on("ready", () => {
     console.log("Bot ONLINE")
